6702269118e220c11c94287cdb66497d85340241
Damien Nelson <dhnisaman@gmail.com>
2022-12-09 15:14:44 -0500

    commit tree 3f1d9e0528969ce365424551cb133c963ad94fcc

--- main.js
+++ main.js
@@ -3,7 +3,64 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const failed = document.querySelector("body div")
 
+const hearts = document.querySelectorAll(".like-glyph")
+hearts.forEach((heart) => {
+  heart.addEventListener('click', (e) => {
+    if(e.target.classList.value === 'like-glyph'){
+      // e.target.textContent = FULL_HEART
+      // e.target.classList.add('activated-heart')
+      mimicServerCall("http://mimicServer.example.com", {
+        method: 'POST',
+        Headers: {
+          "content-type": "application/jason",
+          "accept": "application/jason",
+        },
+        body: JSON.stringify({
+          Heart: 'FULL_HEART',
+          Color: "activated-heart"
+        }),
+      })
+      .then((resp) => {
+        e.target.textContent = FULL_HEART
+        e.target.classList.add('activated-heart')
+      })
+      .catch((error) => {
+        console.log(resp)
+        setTimeout(() => {
+          failed.classList.add('hidden')
+        },1000)
+        failed.classList.remove('hidden')
+      })
+    }else{
+    //   e.target.textContent = EMPTY_HEART
+    //  e.target.classList.remove('activated-heart')
+     mimicServerCall("http://mimicServer.example.com", {
+      method: 'POST',
+      Headers: {
+        "content-type": "application/jason",
+        "accept": "application/jason",
+      },
+      body: JSON.stringify({
+        Heart: 'EMPTY_HEART',
+        Color: "activated-heart"
+      }),
+    })
+    .then((resp) => {
+      e.target.textContent = EMPTY_HEART
+      e.target.classList.remove("activated-heart")
+    })
+    .catch((resp) => {
+      console.log(resp)
+      setTimeout(() => {
+        failed.classList.add('hidden')
+      },1000)
+      failed.classList.remove('hidden')
+    })
+    }
+  })
+})
 
 
 
